ae53b04604d65708ca2f205fdaa844ed6e4824ae
masa <masa016@gmail.com>
2020-08-02 00:50:55 +0900

    no message

---
+++ myblog/myblog/settings/production.py
@@ -0,0 +1,18 @@
+from .base import *
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['118.27.16.78']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myblog',
+        'USER': 'root',
+        'PASSWPRD': '123456789',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+    }
+}
